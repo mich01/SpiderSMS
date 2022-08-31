9b79903a5a0ccaeedf908ff6adf1274f88929c57
Mich01 <mich01mk@gmail.com>
2022-08-31 04:01:08 +0300

    Fixed Multiple Permissions Request, Fixed Startup freeze from first install, Fixed DefaultSMS app settings Request Version 10 on Playstore

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -13,3 +13,13 @@ o
 ?app/src/main/java/com/mich01/spidersms/Setup/SetupActivity.java,6\2\62eb3a6518e9585f6e49add9ba282a5201e1c5f0
 m
 =app/src/main/java/com/mich01/spidersms/Setup/SetupConfig.java,4\6\46e3dc14c344af57937ddfd17d0057336157b379
+D
+google-services.json,f\7\f761570c4ae90a5577cea77748c77672342c8041
+C
+workflows/build.yml,7\7\770e8b7d3c28c272840fdca49157f9d51b2c7985
+t
+Dapp/src/main/java/com/mich01/spidersms/Receivers/BackupReceiver.java,a\c\acb5b46e3e60a02b2d313ea4454aca70d475c399
+q
+Aapp/src/main/java/com/mich01/spidersms/Receivers/MMSReceiver.java,2\8\28151946b7137a584db50552e012142ed24324dc
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
--- app/src/main/java/com/mich01/spidersms/SplashActivity.java
+++ app/src/main/java/com/mich01/spidersms/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.mich01.spidersms;
 
 
+import static android.telecom.TelecomManager.ACTION_CHANGE_DEFAULT_DIALER;
+import static android.telecom.TelecomManager.EXTRA_CHANGE_DEFAULT_DIALER_PACKAGE_NAME;
 import static com.mich01.spidersms.Crypto.PKI_Cipher.GeneratePrivateKey;
 import static com.mich01.spidersms.Data.StringsConstants.SetupComplete;
 import static com.mich01.spidersms.Prefs.PrefsMgr.MyPrefs;
@@ -8,15 +10,24 @@ import static com.mich01.spidersms.Prefs.PrefsMgr.getPrefs;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.role.RoleManager;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.provider.Telephony;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
@@ -24,12 +35,88 @@ import com.mich01.spidersms.Backend.BackendFunctions;
 import com.mich01.spidersms.Setup.SetupActivity;
 import com.mich01.spidersms.UI.UnlockActivity;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
 import java.util.Objects;
 
 @SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity
 {
     int First_Run = 0;
+    AlertDialog alertDialog;
+    int permissionsCount = 0;
+    ArrayList<String> permissionsList;
+    final String[] appPermissions =new String[]{Manifest.permission.SEND_SMS,Manifest.permission.READ_CONTACTS,Manifest.permission.CAMERA,Manifest.permission.RECEIVE_SMS,Manifest.permission.READ_EXTERNAL_STORAGE};
+    ActivityResultLauncher<String[]> permissionsLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(),
+                    new ActivityResultCallback<Map<String, Boolean>>() {
+                        @RequiresApi(api = Build.VERSION_CODES.M)
+                        @Override
+                        public void onActivityResult(Map<String, Boolean> result) {
+                            ArrayList<Boolean> list = new ArrayList<>(result.values());
+                            permissionsList = new ArrayList<>();
+                            permissionsCount = 0;
+                            for (int i = 0; i < list.size(); i++) {
+                                if (shouldShowRequestPermissionRationale(appPermissions[i])) {
+                                    permissionsList.add(appPermissions[i]);
+                                } else if (!hasPermission(SplashActivity.this, appPermissions[i])) {
+                                    permissionsCount++;
+                                }
+                            }
+                            if (permissionsList.size() > 0) {
+                                askForPermissions(permissionsList);
+                            } else if (permissionsCount > 0) {
+                                showPermissionDialog();
+                            } else {
+                                checkInstallationStatus();
+                            }
+                        }
+                    });
+    ActivityResultLauncher<Intent> defaultSMSActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK)
+                {
+                    permissionsList = new ArrayList<>();
+                    permissionsList.addAll(Arrays.asList(appPermissions));
+                    askForPermissions(permissionsList);
+                }
+                else if (result.getResultCode() == Activity.RESULT_CANCELED)
+                {
+                    permissionsList = new ArrayList<>();
+                    permissionsList.addAll(Arrays.asList(appPermissions));
+                    askForPermissions(permissionsList);
+                }
+            });
+    private boolean hasPermission(Context context, String permissionStr) {
+        return ContextCompat.checkSelfPermission(context, permissionStr) == PackageManager.PERMISSION_GRANTED;
+    }
+    private void showPermissionDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permission required")
+                .setMessage("You need to Allow these permissions to use this app")
+                .setPositiveButton("Ok", (dialog, which) -> {
+                    dialog.dismiss();
+                });
+        if (alertDialog == null) {
+            alertDialog = builder.create();
+            if (!alertDialog.isShowing()) {
+                alertDialog.show();
+            }
+        }
+    }
+    private void askForPermissions(ArrayList<String> permissionsList) {
+        String[] newPermissionStr = new String[permissionsList.size()];
+        for (int i = 0; i < newPermissionStr.length; i++) {
+            newPermissionStr[i] = permissionsList.get(i);
+        }
+        if (newPermissionStr.length > 0) {
+            permissionsLauncher.launch(newPermissionStr);
+        } else {
+            showPermissionDialog();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -37,9 +124,10 @@ public class SplashActivity extends AppCompatActivity
         setContentView(R.layout.activity_splash); Objects.requireNonNull(getSupportActionBar()).hide();
         if (!BackendFunctions.checkRoot() || BackendFunctions.checkRoot())
         {
-           checkInstallationStatus();
+            checkInstallationStatus();
         }
-        else {
+        else
+        {
             AlertDialog alertDialog = new AlertDialog.Builder(SplashActivity.this).create();
             alertDialog.setTitle(R.string.error);
             alertDialog.setIcon(R.drawable.ic_baseline_error_outline_24);
@@ -53,7 +141,42 @@ public class SplashActivity extends AppCompatActivity
             alertDialog.show();
         }
     }
+    private void checkIfDefaultSMSApp()
+    {
+        if(!getPackageName().equals(getPackageName().equals(Telephony.Sms.getDefaultSmsPackage(this))))
+        {
+            DialogInterface.OnClickListener dialogClickListener = (dialog, which) -> {
+                switch (which)
+                {
+                    case DialogInterface.BUTTON_POSITIVE:
+                        RoleManager roleManager = null;
+                        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+                            roleManager = (RoleManager) getSystemService(Context.ROLE_SERVICE);
+                            Intent intent = roleManager.createRequestRoleIntent(RoleManager.ROLE_SMS);
+                            defaultSMSActivityResultLauncher.launch(intent);
+                        }
+                        else
+                        {
+                            Intent intent = new Intent(ACTION_CHANGE_DEFAULT_DIALER)
+                                    .putExtra(EXTRA_CHANGE_DEFAULT_DIALER_PACKAGE_NAME, getPackageName());
+                            startActivity(intent);
+                            dialog.dismiss();
+                        }
+                        break;
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        permissionsList = new ArrayList<>();
+                        permissionsList.addAll(Arrays.asList(appPermissions));
+                        askForPermissions(permissionsList);
+                        dialog.dismiss();
+                        break;
+                }
+            };
 
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(R.string.set_default_sms_message).setPositiveButton("Yes", dialogClickListener)
+                    .setNegativeButton("No", dialogClickListener).show();
+        }
+    }
     private void checkInstallationStatus() {
         MyPrefs = getPrefs(this);
         if (MyPrefs.getLong("InstalledTimestamp", 0) == 0)
@@ -63,26 +186,11 @@ public class SplashActivity extends AppCompatActivity
             PrefEditor = MyPrefs.edit();
             PrefEditor.putLong("InstalledTimestamp", System.currentTimeMillis());
             PrefEditor.apply();
-            CheckPermissions();
+            checkIfDefaultSMSApp();
         }else
         {
-            CheckPermissions();
-        }
-    }
-
-    public void CheckPermissions()
-    {
-        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.SEND_SMS)  != PackageManager.PERMISSION_GRANTED ||
-                ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_CONTACTS)  != PackageManager.PERMISSION_GRANTED ||
-                ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECEIVE_SMS)  != PackageManager.PERMISSION_GRANTED ||
-                ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA)  != PackageManager.PERMISSION_GRANTED ||
-                ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE)  != PackageManager.PERMISSION_GRANTED )
-        {
-            RequestPermissions();
-        }
-        else
-        {
-            new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            new Handler(Looper.getMainLooper()).postDelayed(() ->
+            {
                 MyPrefs = getPrefs(this);
                 First_Run=MyPrefs.getInt(SetupComplete, 0);
                 Intent i;
@@ -98,10 +206,5 @@ public class SplashActivity extends AppCompatActivity
                 finish();
             }, 1000);
         }
-    }
-    public void RequestPermissions()
-    {
-            requestPermissions(new String[]{Manifest.permission.SEND_SMS,Manifest.permission.READ_CONTACTS,Manifest.permission.CAMERA,Manifest.permission.RECEIVE_SMS,Manifest.permission.READ_EXTERNAL_STORAGE},1111);
-        CheckPermissions();
     }
 }
